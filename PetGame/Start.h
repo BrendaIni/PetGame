f6ca24d22b9ceacc8cc6b61b873c1a6df36ae550
Brenda Iñiguez <brenda@Brendas-MacBook-Air.local>
2016-06-23 20:36:26 -0500

    Initial version

--- PetGame/Start.h
+++ PetGame/Start.h
@@ -4,11 +4,11 @@
 //
 //  Created by Brenda Iñiguez on 6/23/16.
 //  Copyright (c) 2016 UAGMobile. All rights reserved.
-//
+///Users/brenda/Documents/PetGame/PetGame/ViewController.h
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface Start : UIViewController
 
 
 @end
